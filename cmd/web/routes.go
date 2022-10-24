db846849d394e54a25c9e8dadbec7bb07d978db9
Paweł Siwoń <siwonpawel@gmail.com>
2022-10-24 18:21:41 +0200

    add middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -12,5 +16,6 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippedCreate)
 
-	return mux
+	chain := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return chain.Then(mux)
 }
