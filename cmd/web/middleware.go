e9c7a2c2e3b3a511823ee810eb4a038f0b2faf3e
Paweł Siwoń <siwonpawel@gmail.com>
2022-11-01 20:14:14 +0100

    use request context for authorization

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -62,4 +63,27 @@ func (app *application) noSurf(next http.Handler) http.Handler {
 	})
 
 	return crsfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserId")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
