0210729e9312f7f735cc9430229e735d154fdf44
Paweł Siwoń <siwonpawel@gmail.com>
2022-11-16 18:32:48 +0100

    use of generic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -68,7 +68,7 @@ func (app *application) snippedCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 365, 7, 1), "expires", "This field must be equal 1, 7 or 365")
+	form.CheckField(validator.PermittedValue(form.Expires, 365, 7, 1), "expires", "This field must be equal 1, 7 or 365")
 
 	if !form.Valid() {
 		data := app.newTemplateData(r)
