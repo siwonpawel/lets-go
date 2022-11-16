26404e8db79e58675a978ecb67a5829ddb14b9f5
Paweł Siwoń <siwonpawel@gmail.com>
2022-11-16 22:25:47 +0100

    test signup post

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"testing"
 	"time"
@@ -75,6 +76,22 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	}
 
 	bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
 	return rs.StatusCode, rs.Header, string(body)
 }
 
